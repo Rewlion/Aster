9afb70c6a4eeec20f334da009b4f5f72b93dbca4
Rewlion <mighty.rewlion@gmail.com>
2021-11-04 19:35:53 +0300

    engine: add gnu bison, flex as de[

--- src/engine/datablock/parser/generate.bat
+++ src/engine/datablock/parser/generate.bat
@@ -1,2 +1,2 @@
-win_flex -o lex.yy.cpp parser.l
-win_bison %1 -d -o parser.tab.cpp parser.y
+..\..\..\..\deps\bison\win_flex -o lex.yy.cpp parser.l
+..\..\..\..\deps\bison\win_bison %1 -d -o parser.tab.cpp parser.y
