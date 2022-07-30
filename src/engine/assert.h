6667a239c7ab69d8febbb710bab382a856d2908b
Rewlion <mighty.rewlion@gmail.com>
2022-07-30 16:01:51 +0300

    engine: add fmt to assert

--- src/engine/assert.h
+++ src/engine/assert.h
@@ -2,14 +2,16 @@
 
 #include <engine/types.h>
 
-#define ASSERT_COMMON(cond, isIgnorable)\
+#define ASSERT_COMMON(cond, msg, isIgnorable)\
         do{\
           if ((cond) == false)\
-            show_assert_window(#cond, isIgnorable);\
+            show_assert_window(msg, isIgnorable);\
         } while(false)
 
-#define ASSERT(cond) ASSERT_COMMON(cond, false)
+#define ASSERT(cond) ASSERT_COMMON(cond, #cond, false)
 
-#define IASSERT(cond) ASSERT_COMMON(cond, true)
+#define ASSERT_FMT(cond, f, ...) ASSERT_COMMON(cond, fmt::format(f, __VA_ARGS__), false)
+
+#define IASSERT(cond) ASSERT_COMMON(cond, #cond, true)
 
 void show_assert_window(string error, const bool isIgnorable = false);
