ede8472b37f9638dcd0be15c675d4df0ac764609
Rewlion <mighty.rewlion@gmail.com>
2022-11-02 00:03:53 +0300

    engine: tweak assert inside settings loading

--- src/engine/settings.cpp
+++ src/engine/settings.cpp
@@ -1,8 +1,9 @@
 #include "settings.h"
 
+#include <engine/assert.h>
 #include <engine/datablock/datablock.h>
 #include <engine/datablock/utils.h>
-#include <assert.h>
+#include <engine/log.h>
 
 namespace Engine
 {
@@ -10,8 +11,8 @@ namespace Engine
 
   void load_app_settings(const char* blkPath)
   {
-    if (!load_blk_from_file(&SETTINGS_BLK, blkPath))
-      assert("failed to load blk");
+    ASSERT_FMT(load_blk_from_file(&SETTINGS_BLK, blkPath),
+      "failed to load application settings from {}", blkPath);
   }
 
   DataBlock* get_app_settings()
