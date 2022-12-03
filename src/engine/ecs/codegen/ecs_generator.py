0b198eba43306cbc3b779e696c5edd301a0e8270
Rewlion <mighty.rewlion@gmail.com>
2022-12-03 23:49:32 +0200

    engine, ecs codegen: support defines via args

--- src/engine/ecs/codegen/ecs_generator.py
+++ src/engine/ecs/codegen/ecs_generator.py
@@ -4,6 +4,7 @@ import os.path
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('-D' , nargs=1, help='set c++ define', action="append")
 parser.add_argument('-I' , nargs=1, help='Include path', action="append")
 parser.add_argument('src', help='Source file')
 print(os. getcwd())
@@ -15,10 +16,15 @@ if args.I != None:
   for i in args.I:
     clangIncludes = clangIncludes + ['-I', i[0]]
 
+clangDefines = []
+if args.D != None:
+  for d in args.D:
+    clangDefines = clangDefines + ['-D', d[0]]
+
 if not os.path.isfile(args.src):
   raise ValueError('%s does not exist' % args.src)
 
-generatedSrc = generate_ecs_impl(args.src, clangIncludes)
+generatedSrc = generate_ecs_impl(args.src, clangIncludes, clangDefines)
 
 
 f = open(args.src.replace('.ecs.cpp', '.gen.cpp'), 'w')
