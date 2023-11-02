5841f4b3d034856645d39fc6e7df8854fb1f9a52
Rewlion <mighty.rewlion@gmail.com>
2023-11-02 21:10:33 +0200

    engine, render: rewrote post transparent nodes via dsl

--- src/engine/render/nodes/post_transparent.ecs.cpp
+++ src/engine/render/nodes/post_transparent.ecs.cpp
@@ -1,136 +1,119 @@
 #include <engine/render/debug/debug_text_queue.h>
 #include <engine/render/ecs_utils.h>
 #include <engine/render/font/font_render.h>
-#include <engine/render/frame_graph/frame_graph.h>
+#include <engine/render/frame_graph/dsl.h>
 #include <engine/render/gui_render.h>
 #include <engine/render/imgui/imgui_render.h>
 #include <engine/tfx/tfx.h>
 #include <engine/window.h>
-#include <engine/events.h>
-#include <engine/ecs/macros.h>
 
-ECS_EVENT_SYSTEM()
+NODE_BEGIN(ui)
+  RP_BEGIN()
+    TARGET(final_target)
+  RP_END()
+  EXEC(ui_exec)
+NODE_END()
+
+NODE_EXEC()
 static
-void mk_ui_node(const Engine::OnFrameGraphInit&)
+void ui_exec(gapi::CmdEncoder& encoder)
 {
-  fg::register_node("ui", FG_FILE_DECL, [](fg::Registry& reg)
-  {
-    auto finalFrame = reg.modifyTexture("final_target", gapi::TextureState::RenderTarget);
+  if (auto* guiRender = Engine::Render::get_gui_render())
+    guiRender->render(encoder);
 
-    reg.requestRenderPass()
-       .addTarget(finalFrame, gapi::LoadOp::Load, gapi::StoreOp::Store);
-
-    return [](gapi::CmdEncoder& encoder)
-    {
-      if (auto* guiRender = Engine::Render::get_gui_render())
-        guiRender->render(encoder);
-
-      if (auto* imguiRender = Engine::Render::get_imgui_render())
-        imguiRender->render(encoder);
-    };
-  });
+  if (auto* imguiRender = Engine::Render::get_imgui_render())
+    imguiRender->render(encoder);
 }
 
-ECS_EVENT_SYSTEM()
+NODE_BEGIN(dbg_text)
+  ORDER_ME_AFTER(ui)
+  RP_BEGIN()
+    TARGET(final_target)
+  RP_END()
+  EXEC(dbg_text_exec)
+NODE_END()
+
+NODE_EXEC()
 static
-void mk_dbg_text_node(const Engine::OnFrameGraphInit&)
+void dbg_text_exec(gapi::CmdEncoder& encoder)
 {
-  fg::register_node("dbg_text", FG_FILE_DECL, [](fg::Registry& reg)
+  auto* fontRender = Engine::Render::get_font_render();
+  auto* dbgTextQueue = Engine::Render::dbg::get_dbg_text_queue();
+
+  if (fontRender && dbgTextQueue)
   {
-    reg.orderMeAfter("ui");
-    auto rt = reg.modifyTexture("final_target", gapi::TextureState::RenderTarget);
-
-    reg.requestRenderPass()
-       .addTarget(rt, gapi::LoadOp::Load, gapi::StoreOp::Store);
-
-    return [](gapi::CmdEncoder& encoder)
+    const float textSize = Engine::Window::get_window_size().y * 0.03;
+    float i = 0;
+    for (const auto& e: *dbgTextQueue)
     {
-      auto* fontRender = Engine::Render::get_font_render();
-      auto* dbgTextQueue = Engine::Render::dbg::get_dbg_text_queue();
-
-      if (fontRender && dbgTextQueue)
-      {
-        const float textSize = Engine::Window::get_window_size().y * 0.03;
-        float i = 0;
-        for (const auto& e: *dbgTextQueue)
-        {
-          const float2 pos = {5.0f, textSize * i};
-          fontRender->render(e.text, pos, textSize, e.color, encoder);
-          ++i;
-        }
-      }
-    };
-  });
+      const float2 pos = {5.0f, textSize * i};
+      fontRender->render(e.text, pos, textSize, e.color, encoder);
+      ++i;
+    }
+  }
 }
 
-ECS_EVENT_SYSTEM()
+NODE_BEGIN(TAA)
+  READ_TEX_AS(final_target, AS(taa_current_frame), TEX_STATE(ShaderRead))
+  READ_TEX_PREV_FRAME_AS(final_antialiased_target, AS(taa_prev_frame), TEX_STATE(ShaderRead))
+  READ_TEX(motionBuf, TEX_STATE(ShaderRead))
+  READ_TEX(gbuffer_depth, TEX_STATE(DepthReadStencilRead))
+  CREATE_TEX_2D(final_antialiased_target, TEX_SIZE_RELATIVE(), R8G8B8A8_UNORM, TEX_USAGE3(RT,SRV,TRANSFER_SRC), TEX_STATE(RenderTarget))
+
+  RP_BEGIN()
+    TARGET_LOAD_DONTCARE(final_antialiased_target)
+  RP_END()
+
+  EXEC(taa_exec)
+NODE_END()
+
+NODE_EXEC()
 static
-void mk_taa_node(const Engine::OnFrameGraphInit&)
+void taa_exec(const gapi::TextureHandle taa_current_frame,
+              const gapi::TextureHandle taa_prev_frame,
+              const gapi::TextureHandle motionBuf,
+              const gapi::TextureHandle gbuffer_depth,
+              gapi::CmdEncoder& encoder)
 {
-  fg::register_node("TAA", FG_FILE_DECL, [](fg::Registry& reg)
-  {
-    auto renderSize = Engine::Render::get_render_size();
+  tfx::set_extern("taaCurrentFrame", taa_current_frame);
+  tfx::set_extern("taaPrevFrame", taa_prev_frame);
+  tfx::set_extern("motionBuf", motionBuf);
+  tfx::set_extern("renderSize", encoder.getRenderSize());
+  tfx::set_extern("gbuffer_depth", gbuffer_depth);
+  tfx::activate_technique("TAA", encoder);
 
-    gapi::TextureAllocationDescription allocDesc;
-    allocDesc.format = gapi::TextureFormat::R8G8B8A8_UNORM;
-    allocDesc.extent = int3{renderSize.x, renderSize.y, 1};
-    allocDesc.mipLevels = 1;
-    allocDesc.arrayLayers = 1;
-    allocDesc.usage = gapi::TEX_USAGE_RT | gapi::TEX_USAGE_SRV | gapi::TEX_USAGE_TRANSFER_SRC;
-
-
-    auto taaCurrentFrame = reg.readTexture("final_target", gapi::TextureState::ShaderRead);
-    auto taaPrevFrame = reg.readTexture("final_antialiased_target", gapi::TextureState::ShaderRead, fg::Timeline::Previous);
-    auto motionBuf = reg.readTexture("motionBuf", gapi::TextureState::ShaderRead);
-    auto gbufferDepth = reg.readTexture("gbuffer_depth", gapi::TextureState::DepthReadStencilRead);
-
-    auto rt = reg.createTexture("final_antialiased_target", allocDesc, gapi::TextureState::RenderTarget);
-    reg.requestRenderPass()
-       .addTarget(rt, gapi::LoadOp::DontCare, gapi::StoreOp::Store);
-
-    return [taaCurrentFrame, taaPrevFrame, motionBuf, gbufferDepth](gapi::CmdEncoder& encoder)
-    {
-      tfx::set_extern("taaCurrentFrame", taaCurrentFrame.get());
-      tfx::set_extern("taaPrevFrame", taaPrevFrame.get());
-      tfx::set_extern("motionBuf", motionBuf.get());
-      tfx::set_extern("renderSize", encoder.getRenderSize());
-      tfx::set_extern("gbuffer_depth", gbufferDepth.get());
-      tfx::activate_technique("TAA", encoder);
-
-      encoder.updateResources();
-      encoder.draw(4, 1, 0, 0);
-    };
-  });
+  encoder.updateResources();
+  encoder.draw(4, 1, 0, 0);
 }
 
-ECS_EVENT_SYSTEM()
+NODE_BEGIN(present)
+  READ_TEX(final_antialiased_target, TEX_STATE(TransferSrc))
+  MODIFY_TEX(backbuffer, TEX_STATE(TransferDst))
+  EXEC(present_exec)
+NODE_END()
+
+NODE_EXEC()
 static
-void mk_present_node(const Engine::OnFrameGraphInit&)
+void present_exec(gapi::CmdEncoder& encoder,
+                  const gapi::TextureHandle final_antialiased_target,
+                  const gapi::TextureHandle backbuffer)
 {
-  fg::register_node("present", FG_FILE_DECL, [](fg::Registry& reg)
-  {
-    auto rt = reg.readTexture("final_antialiased_target", gapi::TextureState::TransferSrc);
-    auto backbuffer = reg.modifyTexture("backbuffer", gapi::TextureState::TransferDst);
-    return [rt, backbuffer](gapi::CmdEncoder& encoder)
-    {
-      const auto region = gapi::TextureSubresourceLayers{
-        .aspects = gapi::ASPECT_COLOR,
-        .mipLevel = 0,
-        .baseArrayLayer = 0,
-        .layerCount = 1
-      };
+  const auto region = gapi::TextureSubresourceLayers{
+    .aspects = gapi::ASPECT_COLOR,
+    .mipLevel = 0,
+    .baseArrayLayer = 0,
+    .layerCount = 1
+  };
 
-      const uint3 rtFrameExtent = rt.describe().extent;
+  const uint3 rtFrameExtent = gapi::get_texture_extent(final_antialiased_target);
 
-      const auto blit = gapi::TextureBlit{
-        .srcSubresource = region,
-        .srcOffsets = {int3{0,0,0}, rtFrameExtent},
-        .dstSubresource = region,
-        .dstOffsets = {int3{0,0,0}, rtFrameExtent},
-      };
+  const auto blit = gapi::TextureBlit{
+    .srcSubresource = region,
+    .srcOffsets = {int3{0,0,0}, rtFrameExtent},
+    .dstSubresource = region,
+    .dstOffsets = {int3{0,0,0}, rtFrameExtent},
+  };
 
-      encoder.blitTexture(rt.get(), backbuffer.get(), 1, &blit, gapi::ImageFilter::Nearest);
-      encoder.transitTextureState(backbuffer.get(), gapi::TextureState::TransferDst, gapi::TextureState::Present);
-    };
-  });
+  encoder.blitTexture(final_antialiased_target, backbuffer, 1, &blit, gapi::ImageFilter::Nearest);
+  encoder.transitTextureState(backbuffer, gapi::TextureState::TransferDst, gapi::TextureState::Present);
 }
--- src/engine/render/nodes/post_transparent.gen.cpp
+++ src/engine/render/nodes/post_transparent.gen.cpp
@@ -8,73 +8,132 @@
 
 using namespace ecs;
 
-static void mk_ui_node_internal(Event* event, ComponentsAccessor& accessor)
+//Engine::OnFrameGraphInit handler
+static
+void mk_fg_node_ui(Event*, ComponentsAccessor&)
 {
-  const Engine::OnFrameGraphInit* casted_event = reinterpret_cast<const Engine::OnFrameGraphInit*>(event);
+  fg::register_node("ui", FG_FILE_DECL, [](fg::Registry& reg)
+  { 
+    reg.requestRenderPass()
+      .addTarget("final_target", gapi::LoadOp::Load, gapi::StoreOp::Store, gapi::ClearColorValue{uint32_t{0}})
+    ;
 
-  mk_ui_node(*casted_event);
+
+    return [](gapi::CmdEncoder& encoder)
+    {
+      ui_exec(encoder);
+    };
+  });
 }
 
-
-static EventSystemRegistration mk_ui_node_registration(
-  mk_ui_node_internal,
+static
+EventSystemRegistration mk_fg_node_ui_registration(
+  mk_fg_node_ui,
   compile_ecs_name_hash("OnFrameGraphInit"),
   {
-
   },
-  "mk_ui_node"
+  "mk_fg_node_ui"
 );
 
 
-static void mk_dbg_text_node_internal(Event* event, ComponentsAccessor& accessor)
+//Engine::OnFrameGraphInit handler
+static
+void mk_fg_node_dbg_text(Event*, ComponentsAccessor&)
 {
-  const Engine::OnFrameGraphInit* casted_event = reinterpret_cast<const Engine::OnFrameGraphInit*>(event);
+  fg::register_node("dbg_text", FG_FILE_DECL, [](fg::Registry& reg)
+  { 
+    reg.orderMeAfter("ui");
+    reg.requestRenderPass()
+      .addTarget("final_target", gapi::LoadOp::Load, gapi::StoreOp::Store, gapi::ClearColorValue{uint32_t{0}})
+    ;
 
-  mk_dbg_text_node(*casted_event);
+
+    return [](gapi::CmdEncoder& encoder)
+    {
+      dbg_text_exec(encoder);
+    };
+  });
 }
 
-
-static EventSystemRegistration mk_dbg_text_node_registration(
-  mk_dbg_text_node_internal,
+static
+EventSystemRegistration mk_fg_node_dbg_text_registration(
+  mk_fg_node_dbg_text,
   compile_ecs_name_hash("OnFrameGraphInit"),
   {
-
   },
-  "mk_dbg_text_node"
+  "mk_fg_node_dbg_text"
 );
 
 
-static void mk_taa_node_internal(Event* event, ComponentsAccessor& accessor)
+//Engine::OnFrameGraphInit handler
+static
+void mk_fg_node_TAA(Event*, ComponentsAccessor&)
 {
-  const Engine::OnFrameGraphInit* casted_event = reinterpret_cast<const Engine::OnFrameGraphInit*>(event);
+  fg::register_node("TAA", FG_FILE_DECL, [](fg::Registry& reg)
+  { 
+    const uint2 __renderSize__ = reg.getRenderSize();
 
-  mk_taa_node(*casted_event);
+    auto taa_current_frame = reg.readTexture("final_target", gapi::TextureState::ShaderRead, false);
+    auto taa_prev_frame = reg.readTexture("final_antialiased_target", gapi::TextureState::ShaderRead, fg::Timeline::Previous);
+    auto motionBuf = reg.readTexture("motionBuf", gapi::TextureState::ShaderRead, false);
+    auto gbuffer_depth = reg.readTexture("gbuffer_depth", gapi::TextureState::DepthReadStencilRead, false);
+
+    auto final_antialiased_target = reg.createTexture("final_antialiased_target",
+      gapi::TextureAllocationDescription{
+        .format =          gapi::TextureFormat::R8G8B8A8_UNORM,
+        .extent =          uint3(__renderSize__, 1),
+        .mipLevels =       1,
+        .arrayLayers =     1,
+        .samplesPerPixel = gapi::TextureSamples::s1,
+        .usage =           (gapi::TextureUsage)((gapi::TextureUsage)(gapi::TextureUsage::TEX_USAGE_RT | gapi::TextureUsage::TEX_USAGE_SRV) | gapi::TextureUsage::TEX_USAGE_TRANSFER_SRC)
+      },
+      gapi::TextureState::RenderTarget,
+      false
+    );
+
+    reg.requestRenderPass()
+      .addTarget(final_antialiased_target, gapi::LoadOp::DontCare, gapi::StoreOp::Store, gapi::ClearColorValue{uint32_t{0}})
+    ;
+
+
+    return [taa_current_frame,taa_prev_frame,motionBuf,gbuffer_depth](gapi::CmdEncoder& encoder)
+    {
+      taa_exec(taa_current_frame.get(), taa_prev_frame.get(), motionBuf.get(), gbuffer_depth.get(), encoder);
+    };
+  });
 }
 
-
-static EventSystemRegistration mk_taa_node_registration(
-  mk_taa_node_internal,
+static
+EventSystemRegistration mk_fg_node_TAA_registration(
+  mk_fg_node_TAA,
   compile_ecs_name_hash("OnFrameGraphInit"),
   {
-
   },
-  "mk_taa_node"
+  "mk_fg_node_TAA"
 );
 
 
-static void mk_present_node_internal(Event* event, ComponentsAccessor& accessor)
+//Engine::OnFrameGraphInit handler
+static
+void mk_fg_node_present(Event*, ComponentsAccessor&)
 {
-  const Engine::OnFrameGraphInit* casted_event = reinterpret_cast<const Engine::OnFrameGraphInit*>(event);
+  fg::register_node("present", FG_FILE_DECL, [](fg::Registry& reg)
+  { 
+    auto final_antialiased_target = reg.readTexture("final_antialiased_target", gapi::TextureState::TransferSrc, false);
+    auto backbuffer = reg.modifyTexture("backbuffer", gapi::TextureState::TransferDst);
 
-  mk_present_node(*casted_event);
+    return [final_antialiased_target,backbuffer](gapi::CmdEncoder& encoder)
+    {
+      present_exec(encoder, final_antialiased_target.get(), backbuffer.get());
+    };
+  });
 }
 
-
-static EventSystemRegistration mk_present_node_registration(
-  mk_present_node_internal,
+static
+EventSystemRegistration mk_fg_node_present_registration(
+  mk_fg_node_present,
   compile_ecs_name_hash("OnFrameGraphInit"),
   {
-
   },
-  "mk_present_node"
+  "mk_fg_node_present"
 );
