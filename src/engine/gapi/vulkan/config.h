e849d8971be021f2fa3e5e551207ca996d3de569
Rewlion <mighty.rewlion@gmail.com>
2023-11-15 00:13:54 +0200

    engine, vulkan: enable VMA asserts via config

--- src/engine/gapi/vulkan/config.h
+++ src/engine/gapi/vulkan/config.h
@@ -3,4 +3,5 @@
 #ifdef DEBUG
   #define CFG_DEBUG_UTILS 1
   #define CFG_DEBUG_VALIDATION 1
+  #define CFG_VMA_ASSERTS 0
 #endif
--- src/engine/gapi/vulkan/vma.h
+++ src/engine/gapi/vulkan/vma.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include "config.h"
+
+#if CFG_VMA_ASSERTS
 #include <engine/assert.h>
+#define VMA_ASSERT ASSERT
 #define VMA_HEAVY_ASSERT ASSERT
+#else
+#define VMA_ASSERT(exp) 
+#define VMA_HEAVY_ASSERT(exp) 
+#endif
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnullability-completeness"
 #include <vma/vk_mem_alloc.h>
