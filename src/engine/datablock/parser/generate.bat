f8f3ce20ae0fcc33e97fb959c292213b310783e4
Rewlion <mighty.rewlion@gmail.com>
2024-01-15 16:56:33 +0300

    engine: bye bye datablock

--- src/engine/datablock/parser/generate.bat
+++
@@ -1,6 +0,0 @@
-pushd ..\..\..\..\
-  set parser_dir=src/engine/datablock/parser
-  deps\bison\win_flex -o %parser_dir%/lex.yy.cpp %parser_dir%/parser.l
-  deps\bison\win_bison %1 -d -o %parser_dir%/parser.tab.cpp %parser_dir%/parser.y
-popd
-
--- src/engine/datablock/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <engine/datablock/datablock.h>
-
-bool load_blk_from_file(DataBlock* blk, const char* path);
