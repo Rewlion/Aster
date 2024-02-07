29ee3c51f50f293207320b5d9894f203e05b98f5
Rewlion <mighty.rewlion@gmail.com>
2024-02-07 21:31:49 +0300

    engine, render: GIBS: tweaked nonlinear transform

--- src/engine/shaders/shaders/gi_surfels/consts.hlsl
+++ src/engine/shaders/shaders/gi_surfels/consts.hlsl
@@ -1,12 +1,10 @@
 #ifndef SURFELS_CONSTS_HLSL
 #define SURFELS_CONSTS_HLSL
 
-#define CELLS_DIM_X                       21
-#define CELLS_DIM_Y                       CELLS_DIM_X
-#define CELLS_DIM_Z                       CELLS_DIM_X
-#define CELLS_DIMS                        uint3(CELLS_DIM_X, CELLS_DIM_Y, CELLS_DIM_Z)
+#define CELLS_DIM                         21
+#define CELLS_DIMS                        uint3(CELLS_DIM, CELLS_DIM, CELLS_DIM)
 #define CELLS_HALF_DIM                    uint3((CELLS_DIMS - 1.0.xxx)/2)
-#define CELLS_DIMS_F                      float3(CELLS_DIM_X, CELLS_DIM_Y, CELLS_DIM_Z)
+#define CELLS_DIMS_F                      float3(CELLS_DIM, CELLS_DIM, CELLS_DIM)
 #define CELL_SIZE                         1.0
 
 #define SURFEL_COUNT_TOTAL                (250 * 1024)
--- src/engine/shaders/shaders/gi_surfels/spatial_hash.hlsl
+++ src/engine/shaders/shaders/gi_surfels/spatial_hash.hlsl
@@ -10,10 +10,7 @@ int4 calcLinearIndex(float3 raw_id)
 //it's basically a cube map sampling like
 int4 calcNonLinearIndex(float3 raw_id, float3 camera_to_wpos, float far_plane)
 {
-  int4 idCascade;
-
-  float3 dir = normalize(raw_id);
-  float3 absDir = abs(dir);
+  float3 absDir = abs(camera_to_wpos);
   float dist = 0;
   float2 uv = float2(0,0);
   int face = 0;
@@ -22,29 +19,30 @@ int4 calcNonLinearIndex(float3 raw_id, float3 camera_to_wpos, float far_plane)
   if (absDir.x > absDir.y && absDir.x > absDir.z)
   {
     dist = abs(camera_to_wpos.x);
-    uv = float2(absDir.z, absDir.y);
-    face = dir.x > 0 ? CASCADE_X : CASCADE_MINUS_X;
+    uv = camera_to_wpos.zy;
+    face = camera_to_wpos.x > 0 ? CASCADE_X : CASCADE_MINUS_X;
   }
   //Y FACES
   else if (absDir.y > absDir.z)
   {
     dist = abs(camera_to_wpos.y);
-    uv = float2(absDir.z, absDir.x);
-    face = dir.y > 0 ? CASCADE_Y : CASCADE_MINUS_Y;
+    uv = camera_to_wpos.zx;
+    face = camera_to_wpos.y > 0 ? CASCADE_Y : CASCADE_MINUS_Y;
   }
   //Z FACES
   else
   {
     dist = abs(camera_to_wpos.z);
-    uv = float2(absDir.x, absDir.y);
-    face = dir.z > 0 ? CASCADE_Z : CASCADE_MINUS_Z;
+    uv = camera_to_wpos.xy;
+    face = camera_to_wpos.z > 0 ? CASCADE_Z : CASCADE_MINUS_Z;
   }
-
-  float2 xy = uv * float2(CELLS_DIM_X - 1.0, CELLS_DIM_Y - 1.0);
+  uv = uv / dist; // project on the cube face
+  uv = uv * 0.5 + 0.5;
+  float2 xy = uv * float2(CELLS_DIM - 1.0, CELLS_DIM - 1.0);
   
-  float nearPlane = CELLS_DIM_Z * CELL_SIZE;
+  float nearPlane = CELLS_DIM * CELL_SIZE;
   //z = znear * (zfar/znear)^(slice/nslices) from DOOM
-  float zSlice = floor(log(dist/ nearPlane) / log(far_plane / nearPlane) * CELLS_DIM_Z);  
+  float zSlice = floor(log(dist/ nearPlane) / log(far_plane / nearPlane) * CELLS_DIM);  
 
   return int4(xy, zSlice, face);
 }
@@ -53,7 +51,6 @@ int4 calcSpatialIndex(float3 camera_to_wpos, float far_plane)
 {
   float3 rawId = camera_to_wpos / CELL_SIZE;
   bool isLinearTransform = all(abs(rawId) < CELLS_HALF_DIM);
-  
   [BRANCH]
   if (isLinearTransform)
     return calcLinearIndex(rawId);
