9e241133b5f40d5203340d8dd2857504b203f5cf
Rewlion <mighty.rewlion@gmail.com>
2024-02-07 14:06:16 +0300

    engine, ecs: added comp getter sugar

--- src/engine/ecs/macros.h
+++ src/engine/ecs/macros.h
@@ -9,9 +9,27 @@
     ECS_QUERY()\
     static\
     void query_name(eastl::function<void(params)>);
+
+  #define ECS_COMP_GETTER_(type, name)\
+    static type& get_ ## name()\
+    {\
+      type* comp;\
+      query_ ## name([&](type& val)\
+      {\
+        comp = &val;\
+      });\
+      return *comp;\
+    }
+
+  #define ECS_COMP_GETTER(type, name)\
+    ECS_DESCRIBE_QUERY(query_ ## name, (type& name));\
+    ECS_COMP_GETTER_(type, name)
+
+
 #else
   #define ECS_QUERY()
   #define ECS_SYSTEM()
   #define ECS_EVENT_SYSTEM()
   #define ECS_DESCRIBE_QUERY(query_name, params) 
+  #define ECS_COMP_GETTER_(type, name) 
 #endif
